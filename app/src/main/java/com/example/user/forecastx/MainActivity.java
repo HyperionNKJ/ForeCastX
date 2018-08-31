e301edae8695051e7dbe466c8d2fead79b53a8e0
HyperionNKJ <kjneo1996@gmail.com>
2018-09-01 02:44:21 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/user/forecastx/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.forecastx;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.main_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
